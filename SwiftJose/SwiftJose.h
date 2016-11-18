2f55d85d01fe92778a2522d2621f75a9dd638024
test <test@tests-iMac.local>
2016-11-18 10:06:24 +0000

    Initial Commit

---
+++ SwiftJose/SwiftJose.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJose.h
+//  SwiftJose
+//
+//  Created by test on 18/11/2016.
+//  Copyright © 2016 rmtmckenzie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJose.
+FOUNDATION_EXPORT double SwiftJoseVersionNumber;
+
+//! Project version string for SwiftJose.
+FOUNDATION_EXPORT const unsigned char SwiftJoseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJose/PublicHeader.h>
+
+
